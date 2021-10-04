1d42dbe953e3d7d33a403b718ead9019eee1ada6
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2021-10-04 00:57:30 -0500

    add spring security

---
+++ src/main/java/miu/edu/activitytracking/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package miu.edu.activitytracking;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable().and()
+                .csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
